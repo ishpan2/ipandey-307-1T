49e1cb11537cf04c4fc21ff49b769f19f1b2f744
ishpan2 <ipandey@calpoly.edu>
2018-10-05 10:56:03 -0700

    Upload proj

---
+++ src/com/cs410/multiplier/Home.java
@@ -0,0 +1,10 @@
+package com.cs410.multiplier;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ip
+ *
+ */
+module multiplier {
+}
